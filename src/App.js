36787516c7dc189c5552d37c1ea00d9651effc3b
wideRuledAZ <danielmartinez0603@gmail.com>
2022-03-01 19:39:13 -0600

    Message

--- src/App.js
+++ src/App.js
@@ -4,21 +4,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Hey. What's up.</p>
+      <marquee>Hey queen. You have done it again. Raised the bar for us all. Hey</marquee>
+        <div>
+            <marquee>this website is alive this website is alive this website is alive this website is alive this website is alive this website is alive this website is alive this website is alive this website is alive this website is alive this website is alive this website is alive this website is alive this website is alive this website is alive this website is alive </marquee>
+            <h2>My first website!!</h2>
+            <p>Check this out! Wow!</p>
+        </div>
+        <p>Paragraph 2</p>
+        <p>Paragraph 3</p>
+
+        <br />
+        <h1>Do you like dogs?</h1>
+        <img src = "https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+        <h2>Here's an example^^</h2>
+        <p>aren't they cute?</p>
+
+        <a href = "https://google.com">Go to Google</a>
     </div>
+  
   );
 }
 
